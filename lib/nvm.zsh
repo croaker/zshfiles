114e970e8f0baefdd52433e8fae059bec979af5f
Daniel Gruenthal <d.gruenthal@gmail.com>
2014-03-27 23:38:45 +0100

    Fixed nvm.zsh if no brew is available

--- lib/nvm.zsh
+++ lib/nvm.zsh
@@ -1,3 +1,11 @@
-if [ -n $(which brew) ]; then
-  source $(brew --prefix nvm)/nvm.sh
+local NVM_PATH=""
+
+if [ -d ~/.nvm ]; then
+  NVM_PATH="~/.nvm"
+elif [ -f $(which brew) ]; then
+  NVM_PATH=$(brew --prefix nvm 2> /dev/null)
+fi;
+
+if [ -d $NVM_PATH ]; then
+  source $NVM_PATH/nvm.sh
 fi
