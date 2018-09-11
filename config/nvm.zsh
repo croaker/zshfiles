04b7d944a5fcc7445c12789353f46d53c56dd1fe
Daniel Gruenthal <d.gruenthal@gmail.com>
2018-09-11 12:08:59 +0200

    Improved nvm integration
    This standardizes the nvm integration and makes the implementation less
    brittle.

--- config/nvm.zsh
+++ config/nvm.zsh
@@ -1,11 +1,9 @@
-local NVM_PATH=""
+local BREW_NVM_PATH="/usr/local/opt/nvm"
 
-if [ -d ~/.nvm ]; then
-  NVM_PATH="~/.nvm"
-elif [ -f "$(which brew)" ]; then
-  NVM_PATH=$(brew --prefix nvm 2> /dev/null)
-fi;
-
-if [ ! -z $NVM_PATH ]; then
-  source $NVM_PATH/nvm.sh
+if [ -d "$BREW_NVM_PATH" ]; then
+  export NVM_DIR=$BREW_NVM_PATH
+else
+  export NVM_DIR="$HOME/.nvm"
 fi
+
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use
